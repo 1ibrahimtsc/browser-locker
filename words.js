f4bf3ab8d14ba88e5c251f12b921403d95849951
Mohammed Ibrahim Khalil <1ibrahimtsc@gmail.com>
2025-05-21 08:05:28 +0600

    Update words.js

--- words.js
+++ words.js
@@ -1,10 +1,24 @@
 const masterWordList = {
   soil1: {
     sentence: "Soil provides necessary nutrients and materials for concrete and interior development.",
-    words: ["provides", "necessary", "nutrients", "materials", "concrete", "interior"]
+    words: [
+      { word: "provides", meaning: "সরবরাহ করে" },
+      { word: "necessary", meaning: "প্রয়োজনীয়" },
+      { word: "nutrients", meaning: "পুষ্টি উপাদান" },
+      { word: "materials", meaning: "উপকরণ" },
+      { word: "concrete", meaning: "কংক্রিট" },
+      { word: "interior", meaning: "অভ্যন্তরীণ" }
+    ]
   },
   hygiene1: {
     sentence: "Adequate hygiene strengthens immunity and reduces tiredness and stress.",
-    words: ["Adequate", "hygiene", "strengthens", "tiredness", "reduce", "stress"]
+    words: [
+  { word: "adequate", meaning: "যথেষ্ট" },
+  { word: "hygiene", meaning: "স্বাস্থ্যবিধি" },
+  { word: "strengthens", meaning: "শক্তিশালী করে" },
+  { word: "tiredness", meaning: "ক্লান্তি" },
+  { word: "reduces", meaning: "হ্রাস করা" },
+  { word: "stress", meaning: "চাপ / মানসিক চাপ" }
+]
   }
 };
